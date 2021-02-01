94740fe755484b3879296a8d764ad7f7cec94b27
Carlos(gfcf14) <carloscuba014@gmail.com>
2021-02-01 14:25:09 -0500

    section3-50: create recipe class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component ({
   selector: 'recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes = [];
+
+  ngOnInit(): void {
+    
+  }
 
 }
