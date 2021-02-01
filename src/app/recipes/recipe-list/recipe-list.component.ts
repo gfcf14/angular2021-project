36f5938c525301df642f7c9e1c388505914003de
Carlos(gfcf14) <carloscuba014@gmail.com>
2021-02-01 16:37:09 -0500

    section 6-87: display recipe on click

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component ({
@@ -7,12 +7,17 @@ import { Recipe } from '../recipe.model';
 })
 
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'this is a test', 'https://www.recipetineats.com/wp-content/uploads/2020/02/Honey-Garlic-Chicken-Breast_5-SQ.jpg')
   ];
 
   ngOnInit(): void {
     
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component ({
   selector: 'recipes',
   templateUrl: './recipes.component.html'
 })
 
-export class RecipesComponent {
+export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe;
 
+  ngOnInit() {
+
+  }
 }
