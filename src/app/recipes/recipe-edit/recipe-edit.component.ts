db590dc0520a093542e9ff33411ce955ee2a55e2
Carlos(gfcf14) <carloscuba014@gmail.com>
2021-02-02 16:15:19 -0500

    section 16-229: show recipe ingredients on edit form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -27,20 +27,35 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  get controls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
   private initForm() {
     let recipeName: string = '';
     let recipePath: string = '';
     let recipeDescription: string = '';
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recService.getRecipe(this.id);
       const { name, imagePath, description } = recipe;
       [ recipeName, recipePath, recipeDescription ] = [ name, imagePath, description ];
+
+      if (recipe['ingredients']) {
+        for (let ing of recipe.ingredients) {
+          recipeIngredients.push(new FormGroup({
+            'name': new FormControl(ing.name),
+            'amount': new FormControl(ing.amount)
+          }));
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipePath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
     });
   }
 }
