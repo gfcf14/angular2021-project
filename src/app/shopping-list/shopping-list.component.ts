15bdb433f619222bea4215c9e2afebacac1544e6
Carlos(gfcf14) <carloscuba014@gmail.com>
2021-02-02 11:18:26 -0500

    section 10-120: implement shopping list service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component ({
   selector: 'app-shopping-list',
@@ -7,16 +8,14 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
-  ];
+  ingredients: Ingredient[] = [];
+
+  constructor(private slService: ShoppingListService) {}
 
   ngOnInit() {
-
-  }
-
-  onIngredientAdded(ing: Ingredient) {
-    this.ingredients.push(ing);
+    this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe((ings: Ingredient[]) => {
+      this.ingredients = ings;
+    })
   }
 }
