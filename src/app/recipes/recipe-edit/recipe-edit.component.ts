d5df5ca0d0af2c126b4866eeeba02378e332e272
Carlos(gfcf14) <carloscuba014@gmail.com>
2021-02-02 12:48:59 -0500

    section 12-166: implement programmatic navigation to edit recipe component

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode: boolean = false;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+    });
+  }
 }
