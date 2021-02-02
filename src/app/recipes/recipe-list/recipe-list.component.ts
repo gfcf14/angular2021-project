b3f8a463d617d959c7d3f821dec0964f4c788a7b
Carlos(gfcf14) <carloscuba014@gmail.com>
2021-02-02 10:22:51 -0500

    section 10-117: link recipes service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component ({
   selector: 'app-recipe-list',
@@ -8,12 +9,12 @@ import { Recipe } from '../recipe.model';
 
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test', 'https://www.recipetineats.com/wp-content/uploads/2020/02/Honey-Garlic-Chicken-Breast_5-SQ.jpg')
-  ];
+  recipes: Recipe[] = [];
+
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
-    
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,11 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-  
+  private recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a test', 'https://www.recipetineats.com/wp-content/uploads/2020/02/Honey-Garlic-Chicken-Breast_5-SQ.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
