b2f305c0f5f8b4e17721f058b68291a0dee44fd5
Carlos(gfcf14) <carloscuba014@gmail.com>
2021-02-03 10:52:04 -0500

    section 19-280: save local data to firebase db

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor()
+  constructor(private http: HttpClient,
+              private recService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recService.getRecipes();
+    return this.http.put('https://ng-course-recipe-book-8feee-default-rtdb.firebaseio.com/recipes.json', recipes)
+    .subscribe(response => { console.log(response); });
+  }
 }
